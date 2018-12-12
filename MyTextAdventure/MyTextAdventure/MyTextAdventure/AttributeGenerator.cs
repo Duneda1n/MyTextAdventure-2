fe1cc56305a21f25dad361dc2bc39574c202c8bd
kokoblok <45045734+kokoblok@users.noreply.github.com>
2018-12-12 20:49:30 +0200

    add Hero,AttributeGenerator

---
+++ MyTextAdventure/MyTextAdventure/MyTextAdventure/AttributeGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextAdventure
+{
+    public class AttributeGenerator
+    {
+        Random myRNG = new Random();
+        public Hero Generate(Hero myHero)
+        {
+            myHero.Strength = myRNG.Next(1, 21);
+            if (myHero.HeroClass.ToLower() == "warrior")
+                myHero.Strength = myHero.Strength + 3;
+            else if (myHero.HeroClass.ToLower() == "wizard")
+                myHero.Strength = myHero.Strength - 3;
+            myHero.Intelligence = myRNG.Next(1, 21);
+            myHero.Dexterity = myRNG.Next(1, 21);
+            myHero.Wisdom = myRNG.Next(1, 21);
+            myHero.Charisma = myRNG.Next(1, 21);
+            myHero.Constitution = myRNG.Next(1, 21);
+            myHero.HitPoints = (myHero.Strength + myHero.Constitution) / 2;
+            return myHero;
+        }
+    }
+}
---
+++ MyTextAdventure/MyTextAdventure/MyTextAdventure/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your character's name?");
+            string name = Console.ReadLine();
+            Hero myAwsomeHero = new Hero(name);
+            Console.WriteLine("What's your character's race");
+            string race = Console.ReadLine();
+            myAwsomeHero.Race = race;
+            Console.WriteLine("What's your character's class");
+            string heroClass = Console.ReadLine();
+            myAwsomeHero.HeroClass = heroClass;
+            AttributeGenerator at = new AttributeGenerator();
+            myAwsomeHero = at.Generate(myAwsomeHero);
+            myAwsomeHero.ToString();
+            Console.WriteLine(myAwsomeHero.ToString());
+            Console.ReadKey();
+        }
+    }
+}
